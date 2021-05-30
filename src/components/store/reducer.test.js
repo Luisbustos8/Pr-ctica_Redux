2947e2ecec96f97aa57f97c13de28151c479663b
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2021-05-30 20:14:25 +0200

    Implementando testing: reducer

--- src/components/store/reducer.test.js
+++ src/components/store/reducer.test.js
@@ -1,9 +1,32 @@
 import { adverts, initialState } from './reducers';
+import { ADVERTS_CREATED_SUCCESS, ADVERTS_LOADED_SUCCESS } from './types';
 
 describe('adverts', () => {
     test('should manage ANY action', () => {
-        const state = initialState;
+        const state = initialState.adverts;
         const action = { type: 'ANY' }
-        const nextState = 
-    })
-})
+        const nextState = adverts(state, action)
+        expect(nextState).toBe(state);
+    });
+    test('should manage ADVERTS_LOADED_SUCCESS action', () => {
+        const state = initialState.adverts;
+        const action = { type: ADVERTS_LOADED_SUCCESS, payload: []}
+        const expectedState = {
+            ...initialState.adverts, 
+            loaded:true, 
+            data: action.payload
+        };
+        const nextState = adverts(state, action)
+        expect(nextState).toEqual(expectedState);
+    });
+    test('should mange ADVERTS_CREATED_SUCCESS action', () => {
+        const state = initialState.adverts;
+        const action = { type : ADVERTS_CREATED_SUCCESS, payload: [] }
+        const expectedState = {
+            ...initialState.adverts, 
+            data: [...state.data, action.payload]
+        };
+        const nextState = adverts(state, action);
+        expect(nextState).toEqual(expectedState)
+    });
+});
